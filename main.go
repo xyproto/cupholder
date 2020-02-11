af59cc233f294af345b3a18cc9463b3c65bda1a2
Alexander F. Rødseth <rodseth@gmail.com>
2020-02-11 13:49:50 +0100

    Improve console output

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/urfave/cli/v2"
 	"github.com/xyproto/cdrom"
 	"github.com/xyproto/textoutput"
@@ -9,35 +8,50 @@ import (
 )
 
 func ejectDevice(o *textoutput.TextOutput, deviceFilename string) error {
+	o.Printf("<darkgray>[<blue>eject<darkgray>]\t\t<darkgray>Ejecting <yellow>%s<darkgray>... <off>", deviceFilename)
+
+	// Opening
 	cd, err := cdrom.NewFile(deviceFilename)
 	if err != nil {
+		o.Print("<red>error:</red> ")
 		return err
 	}
-	fmt.Print(o.LightTags(fmt.Sprintf(
-		"<darkgray>[<blue>%s<darkgray>] <yellow>Ejecting... <off>",
-		deviceFilename)))
+
+	// Ejecting
 	if err := cd.Eject(); err != nil {
-		o.OutputTags("<red>failed</red>")
+		o.Print("<red>error:</red> ")
 		return err
 	}
+
+	// Closing
 	if err := cd.Done(); err != nil {
-		o.OutputTags("<red>failed</red>")
+		o.Print("<red>error:</red> ")
 		return err
 	}
-	o.OutputTags("<green>ok</green>")
+
+	// All done
+	o.Println("<green>ok</green>")
 	return nil
 }
 
 func main() {
 	o := textoutput.New()
-	// quiet output: o.Disable()
 	if appErr := (&cli.App{
 		Name:  "eject",
 		Usage: "eject the CD tray (or other trays, given a device file)",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{Name: "silent", Aliases: []string{"s"}},
+		},
 		Action: func(c *cli.Context) error {
+			// Check if text output should be disabled
+			if c.Bool("silent") {
+				o.Disable()
+			}
+			// Check if any arguments are given
 			if c.NArg() == 0 {
 				return ejectDevice(o, "/dev/cdrom")
 			}
+			// Treat all arguments as device files that shall be ejected
 			var err error
 			for _, deviceFilename := range c.Args().Slice() {
 				err = ejectDevice(o, deviceFilename)
