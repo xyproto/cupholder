f9335c3509684e1493d9e722b66fed1a56d90572
Luois45 <github@louis45.de>
2023-01-25 00:27:43 +0100

    Update main.go

--- main.go
+++ main.go
@@ -12,6 +12,13 @@ import (
 
 const versionString = "cupholder 1.1.0"
 
+func (cd *CD) Mount() error {
+	if _, err := unix.IoctlGetInt(cd.fileDescriptor, CDROMCLOSETRAY); err != nil {
+		return err
+	}
+	return nil
+}
+
 func ejectDevice(o *textoutput.TextOutput, deviceFilename string) error {
 	o.Printf("<darkgray>[<blue>cupholder<darkgray>]\t\t<darkgray>Ejecting <yellow>%s<darkgray>... <off>", deviceFilename)
 
@@ -24,6 +31,34 @@ func ejectDevice(o *textoutput.TextOutput, deviceFilename string) error {
 
 	// Ejecting
 	if err := cd.Eject(); err != nil {
+		o.Print("<red>error:</red> ")
+		_ = cd.Done() // Try closing, but only return the other error
+		return err
+	}
+
+	// Closing
+	if err := cd.Done(); err != nil {
+		o.Print("<red>error:</red> ")
+		return err
+	}
+
+	// All done
+	o.Println("<green>ok</green>")
+	return nil
+}
+
+func mountDevice(o *textoutput.TextOutput, deviceFilename string) error {
+	o.Printf("<darkgray>[<blue>cupholder<darkgray>]\t\t<darkgray>Mounting <yellow>%s<darkgray>... <off>", deviceFilename)
+
+	// Opening
+	cd, err := cdrom.NewFile(deviceFilename)
+	if err != nil {
+		o.Print("<red>error:</red> ")
+		return err
+	}
+
+	// Mounting
+	if err := cd.Close(); err != nil {
 		o.Print("<red>error:</red> ")
 		_ = cd.Done() // Try closing, but only return the other error
 		return err
@@ -72,6 +107,38 @@ func generateEjectionHandler(deviceFilenames []string) func(w http.ResponseWrite
 	}
 }
 
+func generateMountingHandler(deviceFilenames []string) func(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(w, "<!doctype html><html><head><title>Cupholder</title><style>html { height: 100%; } body { height: 100%; background: rgb(95,159,221); background-image: linear-gradient(rgba(95,159,221,1) 0%, rgba(95,159,221,1) 6em, rgba(218,192,168,1) 6em, rgba(224,250,255,1) 100%); margin: 3em; overflow: hidden; } h1 { color: orange; text-shadow: 2px 2px 4px #000F; }</style><body>")
+		fmt.Fprintf(w, "<h1>%s</h1><br>", versionString)
+		for _, deviceFilename := range deviceFilenames {
+			fmt.Fprintf(w, "<strong>[cupholder]</strong>&nbsp;&nbsp;<code>Mounting %s...</code> ", deviceFilename)
+
+			// Opening
+			cd, err := cdrom.NewFile(deviceFilename)
+			if err != nil {
+				fmt.Fprintf(w, "<code>error: %v</code><br>", err)
+			}
+
+			// Mounting
+			if err := cd.Mount(); err != nil {
+				fmt.Fprintf(w, "<code>error: %v</code><br>", err)
+				_ = cd.Done() // Try closing, but only output the other error
+				continue
+			}
+
+			// Closing
+			if err := cd.Done(); err != nil {
+				fmt.Fprintf(w, "<code>error: %v</code><br>", err)
+				continue
+			}
+
+			// All done, for this device filename
+			fmt.Fprintln(w, "<code>ok</code><br>")
+		}
+	}
+}
+
 func main() {
 	o := textoutput.New()
 	if appErr := (&cli.App{
@@ -98,7 +165,8 @@ func main() {
 			}
 			if c.Bool("server") {
 				// Set up a HTTP server that is ready to eject the CD ROM at requests on port 0x0CD0 (3280)
-				http.HandleFunc("/", generateEjectionHandler(deviceFilenames))
+				http.HandleFunc("/eject", generateEjectionHandler(deviceFilenames))
+				http.HandleFunc("/mount", generateMountingHandler(deviceFilenames))
 				o.Println("<blue>Listening for ejection requests at <yellow>http://localhost:3280/<off>")
 				return http.ListenAndServe(":3280", nil)
 			}
